0f85d2cd1840389af5b8e89859e9bec8261b542c
gourav017 <gouravsahu696@gmail.com>
2022-08-26 20:31:04 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/project page/Navbar";
 import Products from "./components/project page/Products";
 import SubNavbar from "./components/project page/SubNavbar";
 
 function App() {
   return <div className="App">
-    <Navbar/>
+   
     <SubNavbar/>
     <Products/>
 
