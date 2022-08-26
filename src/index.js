cc0090e8c1bdfa00f1845276b453e2002326a465
gourav017 <gouravsahu696@gmail.com>
2022-08-26 20:32:07 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
+    
         <App />
-      </ChakraProvider>
+
     </Provider>
   </React.StrictMode>
 );
