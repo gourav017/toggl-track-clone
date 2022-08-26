e8186ce8778df7ebd88c5f2ba3fb1f418bcfd1ac
Lalith <lalithsai101@gmail.com>
2022-08-26 21:03:18 +0530

    Navbar & Footer included

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
