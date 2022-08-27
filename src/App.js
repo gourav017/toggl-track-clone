344dfeb1ff0b790d31b711af469fcd6f547876b4
Lalith Sai <82539645+G-LalithSai@users.noreply.github.com>
2022-08-27 21:12:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
 import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Page_Vivek from "./components/Homepage_vivek/Page_Vivek";
-import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
-      {/* <Navbar />
-      <Footer /> */}
-      <Page_Vivek/>
 
     </div>
   );
